19ff99278add5911322969d4b6a11639d3a738d5
morrillo <gustavo.orrillo@moldeo.coop>
2016-12-26 16:45:08 -0300

    Added last_a_document_generated

--- journal.py
+++ journal.py
@@ -8,6 +8,7 @@ _logger = logging.getLogger(__name__)
 _schema = logging.getLogger(__name__ + '.schema')
 
 class account_journal(osv.osv):
+
     def _get_fp_items_generated(self, cr, uid, ids, fields_name, arg, context=None):
         context = context or {}
         r = {}
@@ -16,9 +17,9 @@ class account_journal(osv.osv):
             res = fp.get_counters() if fp else False
             if res and res[fp.id]:
                 if jou.journal_class_id.afip_code == 1: 
-                    r[jou.id] = res[fp.id]['last_a_sale_document_completed']
+                    r[jou.id] = res[fp.id].get('last_a_sale_document_completed',0)
                 elif jou.journal_class_id.afip_code in [ 6, 11 ]:
-                    r[jou.id] = res[fp.id]['last_b_sale_document_completed']
+                    r[jou.id] = res[fp.id].get('last_b_sale_document_completed',0)
                 else:
                     r[jou.id] = False
             else:
@@ -29,11 +30,40 @@ class account_journal(osv.osv):
 
     _inherit = ["account.journal", "fpoc.user"]
 
+    def _get_last_a_sale_document_completed(self, cr, uid, ids, fields_name, arg, context=None):
+        context = context or {}
+        r = {}
+        for jou in self.browse(cr, uid, ids, context):
+            fp = jou.fiscal_printer_id
+            res = fp.get_counters() if fp else False
+            if res and res[fp.id]:
+                r[jou.id] = res[fp.id].get('last_a_sale_document_completed',0)
+	    else:
+                r[jou.id] = False
+        return r
+
+    def _get_last_b_sale_document_completed(self, cr, uid, ids, fields_name, arg, context=None):
+        context = context or {}
+        r = {}
+        for jou in self.browse(cr, uid, ids, context):
+            fp = jou.fiscal_printer_id
+            res = fp.get_counters() if fp else False
+            if res and res[fp.id]:
+                r[jou.id] = res[fp.id].get('last_b_sale_document_completed',0)
+	    else:
+                r[jou.id] = False
+        return r
+
+
     _columns = {
         'use_fiscal_printer': fields.boolean('Associated to a fiscal printer'),
 
         'fiscal_printer_items_generated': fields.function(_get_fp_items_generated, type='integer', string='Number of Invoices Generated',method=True, 
                             help="Check how many invoices was generated in the printer.", readonly=True),
+        'last_a_sale_document_completed': fields.function(_get_last_a_sale_document_completed, type='integer', string='Number of A Invoices Generated',
+				method=True,   help="Check how many invoices A  were generated in the printer.", readonly=True),
+        'last_b_sale_document_completed': fields.function(_get_last_b_sale_document_completed, type='integer', string='Number of B Invoices Generated',
+				method=True,   help="Check how many B invoices were generated in the printer.", readonly=True),
     }
  
 account_journal()
